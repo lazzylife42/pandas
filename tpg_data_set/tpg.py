2c7cc66643c55a794b30fbdce01aff17b0905483
Don Lazzylife <88842775+lazzylife42@users.noreply.github.com>
2024-12-10 21:18:51 +0100

    comments

--- tpg_data_set/tpg.py
+++ tpg_data_set/tpg.py
@@ -4,22 +4,22 @@ import seaborn as sns
 
 # 1. Charger les données
 file_path = 'montees-par-arret-par-ligne.csv'
-df = pd.read_csv(file_path, delimiter=';')
+df = pd.read_csv(file_path, delimiter=';', ) #  nrow=<int> pour aller plus vite
 pd.options.display.float_format = '{:,.2f}'.format
 sns.set_palette('Set2') 
 
 # 2. Analyse exploratoire
-print("Colonnes disponibles :", df.columns)
+print("Colonnes disponibles :", df.columns) #   display les columns dispos
 print("\nAperçu des données :")
 print(df.head())
 
 # Statistiques descriptives pour `Nombre de Montées`
 print("\nStatistiques descriptives pour 'Nombre de Montées' :")
-print(df['Nombre de Montées'].describe())
+print(df['Nombre de Montées'].describe()) #     donnes des infos sur la columns
 
 # Arrêt avec le plus et le moins de montées
 print("\nArrêt avec le maximum de montées :")
-print(df.loc[df['Nombre de Montées'].idxmax()])
+print(df.loc[df['Nombre de Montées'].idxmax()]) #   renvoie les infos sur líndex max 
 print("\nArrêt avec le minimum de montées :")
 print(df.loc[df['Nombre de Montées'].idxmin()])
 
@@ -36,17 +36,17 @@ mean_by_day = df_cleaned.groupby('Jour Semaine')['Nombre de Montées'].mean()
 
 # **Personnalisation globale des graphiques**
 plt.rcParams.update({
-    'figure.titlesize': 16,
-    'axes.titlesize': 14,
-    'axes.labelsize': 12,
-    'xtick.labelsize': 12,
-    'ytick.labelsize': 12,
-    'legend.fontsize': 12,
-    'figure.figsize': (10, 24)
+    'figure.titlesize': 14,
+    'axes.titlesize': 12,
+    'axes.labelsize': 8,
+    'xtick.labelsize': 8,
+    'ytick.labelsize': 8,
+    'legend.fontsize': 10,
+    'figure.figsize': (16, 9)  # Ratio 16:9
 })
 
 # 5. Visualisation avec subplots
-fig, axes = plt.subplots(3, 1, figsize=(30, 24))
+fig, axes = plt.subplots(3, 1, figsize=(16, 9), gridspec_kw={'height_ratios': [2, 2, 3]})
 
 # Graphique 1 : Montées totales par ligne
 total_by_line.sort_values().plot(kind='bar', ax=axes[0], title="Montées totales par ligne")
@@ -64,7 +64,7 @@ axes[1].ticklabel_format(style='plain', axis='y')  # Désactiver l'écriture sci
 
 # Ajouter les moyennes sur les barres
 for i, v in enumerate(mean_by_day):
-    axes[1].text(i, v, f"{v:,.2f}", ha='center', va='bottom', fontsize=10)
+    axes[1].text(i, v, f"{v:,.2f}", ha='center', va='bottom', fontsize=8)
 
 # Graphique 3 : Montées par jour pour la ligne avec le plus de montées
 top_line = total_by_line.idxmax()
@@ -78,7 +78,7 @@ axes[2].ticklabel_format(style='plain', axis='y')  # Désactiver l'écriture sci
 
 # Ajouter les valeurs sur les points
 for i, v in enumerate(montées_par_jour_top_ligne):
-    axes[2].text(i, v, f"{int(v):,}", ha='center', va='bottom', fontsize=10)
+    axes[2].text(i, v, f"{int(v):,}", ha='center', va='bottom', fontsize=8)
 
 # Ajuster l'espacement entre les graphiques
 plt.tight_layout()
